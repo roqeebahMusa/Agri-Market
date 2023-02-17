820518a17557f4a71dc42b1f46439015b44fcf85
roqeebahMusa <roqeebahmusa19@gmail.com>
2023-02-17 11:40:07 +0100

    updates

--- src/Components/Form/Decision/User/User.jsx
+++ src/Components/Form/Decision/User/User.jsx
@@ -1,33 +1,33 @@
 import React, {useState} from 'react'
 import UserInput from "../User/UserInput"
 import "./User.css"
-import AG from "../../../../assets/AG.png"
+// import AG from "../../../../assets/AG.png"
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios';
 // import Header from '../../Header/Header';
 
-function SignUp() {
+import ClipLoader from "react-spinners/ClipLoader";
+
+function User() {
   const navigate = useNavigate()
+  const [spin, setSpin] =useState(false)
 
   const [ value, setValues] = useState({
-    firstName: "",
+   firstName: "",
     lastName: "",
     email: "",
     phoneNumber: "",
-    location: "",
+    location:"",
     password:"",
     admin: false,
   });
 
-    const {firstName, lastName, email, phoneNumber, location, password} = value
-
-  // console.log(firstName, lastName, email, phoneNumber)
- 
+  
  
 
   const inputs = [{
     id:1,
-    placeholder: "First Name",
+    placeholder: " First Name",
     type: "text",
     name: "firstName",
     // value: value.name,
@@ -36,84 +36,103 @@ function SignUp() {
     pattern: `[a-zA-Z][a-zA-Z0-9-_. ]{3,20}`
   
   },
-{
-  id:2,
-  placeholder: "Last Name",
-  type: "text",
-  name:"lastName",
-  required: true,
-},
+  {
+    id:2,
+    placeholder: " Last Name",
+    type: "text",
+    name: "lastName",
+    // value: value.name,
+    errMsg: "it has to be at least 4 characters and not more than 20 characters",
+    required: true,
+    pattern: `[a-zA-Z][a-zA-Z0-9-_. ]{3,20}`
+  
+  },
 {
   id:3,
-  placeholder: "Email",
+  placeholder: "  E-mail",
   type: "text",
   name:"email",
+  // value: value.email,
+  errMsg: "must be a valid email",
   required: true,
-  errMsg:"Must be a valid email",
 },
 {
-  id: 4,
-  placeholder: "Phone Number",
-  type:"number",
-  name: "phoneNumber",
+  id:4,
+  placeholder: "  Phone Number",
+  type: "Number",
+  name:"phoneNumber",
+  // value: value.address,
+  required: true,
+  errMsg:"Must be a valid phone number",
+  pattern: "[0-9]{3}-[0-9]{2}-[0-9]{3}",
+},
+{
+  id: 5,
+  placeholder: " Location",
+  type: "text",
+  name: "location",
   required: true,
 },
-
-  {
-    id:5,
-    placeholder: "Location",
-    type: "text",
-    name: "location",
-    required: true,
-  },
-
-  {
+{
   id:6,
-  placeholder: "Password",
+  placeholder: "  Password",
   type: "password",
   name:"password",
+  errMsg: "must all be numbers",
   // value: value.password,
   required: true,
-  pattern: `^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$` 
-  },
-
+},
 ]
 
+console.log(value)
 const handleSubmit = async (event) => {
   try {
+    setSpin(true)
     event.preventDefault();
-  console.log("Created")
-    const response = await axios.post("https://agri-market.onrender.com/api/user", {firstName: firstName, lastName: lastName, email: email, phoneNumber: phoneNumber, location: location,  password:  password});
+    const config = {
+      headers:{
+        "Content-Type":"application/json"
+      }
+    }
+    const response = await axios.post("https://agri-market.onrender.com/api/user", value, config);
     console.log(response);
-    navigate ('/Login')
+    if(response.status === 201) {
+      navigate('/Login')
+    }
+    
   } catch (error) {
-
+    if(error) {
+      alert(error.response.data.message)
+      window.location.reload()
+    }
+      console.log("error message",  error)
+      console.log("response error", error.response.data.message)
   }
 };
 
-
+// useEffect(() => {   
+// }[value])
 const handleChange=(i)=>{
   setValues({...value, [i.target.name]:i.target.value})
 }
 
-const receiveValues =(i)=>{
-  i.preventDefault();
-  console.log(value)
-}
+// const receiveValues =(i)=>{
+//   i.preventDefault();
+//   console.log(value)
+//   window.location.reload()
+// }
 
+console.log(value)
 
 
 
   return (
-    
     <div className='sign_main'>
-{/* <Header /> */}
-
-      <form onSubmit={receiveValues} className='sign_form'>
-        <img src={AG} alt="" className='signlogo' />
+      <form  className='sign_form' onSubmit={handleSubmit}>
+        {/* <img src={Lg} alt="" className='signlogo' /> */}
       <div className="sign_wrap_text">
-        <h2>Sign up</h2><br/>
-        <p fontsize="12px">Please complete the registration form to be an Agri Market Customer</p><br/>
+        <h2>User Sign Up</h2><br/>
+        <p className='user_writeup'>Please complete the registration form to purchase livestock product on Agri-Market platform</p><br/>
         </div>
          <div className=" mnbv">
          
@@ -123,14 +142,26 @@ const receiveValues =(i)=>{
           )}
          </div>
          <div className='check_cont'>
-         <input type="checkbox" onChange={() => setValues ({...value, admin:true})}/> <p className='check_text'>I agree to the Terms of service and privacy of policy of Agri market </p>
+         <input type="checkbox" onChange={() => setValues({...value, admin:false})}/> <p className='check_text'>I agree to the Terms of service and privacy of policy of Agri market </p>
          </div>
         
-        <button type='submit' className='zaw' onClick={handleSubmit}>Submit</button>
-        <p>Already have an account ? <span className="signlogin" >Login</span></p>
+        <button type='submit' className='zaw'>
+        {spin ? (
+ <ClipLoader
+ color='#ffffff'
+ loading={spin}
+ size={15}
+ aria-label="Loading Spinner"
+ data-testid="loader"
+/>
+              ) : 'Submit'}
+        </button>
+
+        <p>Already have an account ? <span className="signlogin" onClick={()=> navigate('/Login')}>Login</span></p>
       </form>
+      <button className='back' onClick={() => navigate('/Choose')}>Go Back</button>
     </div>
   )
 }
 
-export default SignUp
+export default User
