51f5f26bd408a9772567ca6f622b7a30afee9b6b
roqeebahMusa <roqeebahmusa19@gmail.com>
2023-02-17 11:59:32 +0100

    updates

--- src/Components/Header/Dropdown.jsx
+++ src/Components/Header/Dropdown.jsx
@@ -1,20 +1,35 @@
 import React from 'react'
 import "./Header.css"
 import {useNavigate} from "react-router-dom"
-
-
+import { BsCart4 } from "react-icons/bs";
 
 function Dropdown(prop) {
-  const {props} = prop
+
+  const {props} = prop  
     const navigate = useNavigate();
+    const getResponse = JSON.parse(localStorage.getItem("response"));
+
+     const logout = () => {
+      localStorage.removeItem("response" );
+     }
+     
   return (
     <div className='drop_main'>
      <div className='drop_wrap'>
 <p className='dipdrop' onClick={() => {props(); navigate('/')}}>Home</p><br/>
 <p className='dipdrop' onClick={() => {props(); navigate('/MarketPlace')}}>Market Place</p><br/>
-<p className='dipdrop' onClick={() => {props(); navigate('/Cart')}}>Cart</p><br/>
-<p className='dipdrop' onClick={() => {props(); navigate('/Choose')}}>Sign up</p><br/>
-<p className='dipdrop' onClick={() => {props(); navigate('/login')}}>Log in</p><br/>
+<p className='dipdrop' onClick={() => {props(); navigate('/Cart')}}>Cart <BsCart4 /></p><br/>
+
+
+ 
+{ getResponse ? <> <p className='dipdrop' onClick={() => {props(); navigate('/Dashboard')}}>Dashboard</p> <br/>
+<p className='dipdrop' onClick={() => {logout(); navigate('/')}} > Log Out
+</p> </>
+ :
+
+ <> <p className='dipdrop' onClick={() => {props(); navigate('/Choose')}}>Sign up</p><br/>
+  <p className='dipdrop' onClick={() => {props(); navigate('/login')}}>Log in</p><br/></>}
+
     </div>
     </div>
   )
