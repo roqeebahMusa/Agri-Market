008c33ba9509e27f93a76c491a8e5c404d0c1ea7
roqeebahMusa <roqeebahmusa19@gmail.com>
2023-02-17 11:48:15 +0100

    updates

--- src/Components/Form/Login/LoginInputs.jsx
+++ src/Components/Form/Login/LoginInputs.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Login.css"
 
 function LoginInputs(props) {
+  const [focused, setFocused] = useState(false);
+
+  const doFocused =()=>{
+    setFocused(true)
+  }
+  console.log(props.handleChange)
   return (
   
     <div className="loginkkk">
@@ -9,12 +15,15 @@ function LoginInputs(props) {
         <input
          type={props.type}
          name={props.name}
+         onChange={props.handleChange}
          placeholder={props.placeholder}
          required={props.required}
          pattern={props.pattern}
-         
+         focus={focused.toString()}
+         onBlur={doFocused}
          className="loginInput"
         />
+        <span className="errr">{props.errMsg}</span>
         
     </div>
   
